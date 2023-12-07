26b72c58fad17c5f10c30f878fa430e5bdb6a5ff
Alexander Sporn <github@alexsporn.de>
2023-12-07 14:17:19 +0100

    Do not publish pm2.5 state if it is reported as 0xFFFF (happens while device is turned off since it only updates every couple of minutes)

--- esphome/components/levoit/sensor/levoit_sensor.cpp
+++ esphome/components/levoit/sensor/levoit_sensor.cpp
@@ -10,7 +10,7 @@ void LevoitSensor::setup() {
   this->parent_->register_listener(LevoitPayloadType::STATUS_RESPONSE, [this](uint8_t *payloadData, size_t payloadLen) {
     if (this->purpose_ == LevoitSensorPurpose::PM25) {
       if (payloadData[12] == 0xFF && payloadData[13] == 0xFF) {
-        this->publish_state(NAN);
+        //this->publish_state(NAN);
       } else {
         this->publish_state((float) ((payloadData[13] << 8) + payloadData[12]) / 1.0);
       }
